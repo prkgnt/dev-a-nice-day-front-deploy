220546a51628666a194d01f28aa78e2179d779db
prkgnt <pgt258258@gmail.com>
2024-08-06 08:13:59 +0000

    See https://github.com/no-commit-today/tech-swipe-front/commit/e1131b3770f30dbaeac5c6c3ef46ab0ae1a2dd3e from refs/heads/develop

--- output/build.sh
+++
@@ -1,4 +0,0 @@
-cd ../
-mkdir output
-cp -R ./tech-swipe-front/* ./output
-cp -R ./output ./tech-swipe-front/
--- output/src/app/(home)/@modal/(.)onboarding/page.tsx
+++
@@ -1,5 +0,0 @@
-import OnboardingModal from "@/app/_components/OnboardingModal";
-
-export default function Modal() {
-  return <OnboardingModal />;
-}

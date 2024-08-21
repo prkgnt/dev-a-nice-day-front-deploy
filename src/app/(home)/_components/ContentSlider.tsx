3d4cdebd4483da000f9f073077d91c799cbbdd01
prkgnt <pgt258258@gmail.com>
2024-08-21 08:56:51 +0000

    See https://github.com/no-commit-today/tech-swipe-front/commit/b3a3e620467e0c4f18ffa85a47927dcc3502e9a3 from refs/heads/deploy

--- src/app/(home)/_components/ContentSlider.tsx
+++ src/app/(home)/_components/ContentSlider.tsx
@@ -6,65 +6,26 @@ import "swiper/css";
 import IndexIndicator from "./IndexIndicator";
 import Image from "next/image";
 import { Categories } from "@/app/_components/Categories";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import {
-  BASE_URL,
-  getContentsCount,
-  getShuffledContents,
-} from "@/app/_utils/api";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { BASE_URL, getShuffledContents } from "@/app/_utils/api";
 import { useEffect, useState } from "react";
 import { Mousewheel } from "swiper/modules";
 import useParams from "@/app/_hooks/useParams";
 import { IContentData } from "@/app";
 import no_image from "@/../public/assets/no_image.svg";
+import getRandomNumber from "@/app/_utils/getRandomNumber";
 
-export default function ContentSlider() {
+export default function ContentSlider({
+  initialData,
+  contentsCountData,
+}: {
+  initialData: { pages: { content: IContentData[] }[]; pageParams: number[] };
+  contentsCountData: { count: number };
+}) {
   // 몇페이지 전에 패치할 것인지.
   const pagesBeforeFetch = 3;
   const searchParams = useParams("categories").getParamsToString();
-  const [contentsData, setContentsData] = useState<IContentData[] | null>(null);
   const [scrollPosition, setScrollPosition] = useState(0);
-
-  const getRandomNumber = (allPageParams: number[]) => {
-    if (contentsCountData) {
-      // 전체 콘텐츠 개수를 통해 전체 페이지 개수를 계산
-      let pagesCount = Math.ceil(contentsCountData.count / 10);
-
-      // 전체 페이지 개수를 통해 전체 페이지 배열 생성
-      let totalPageArray = Array.from({ length: pagesCount }, (_, i) => i + 1);
-
-      // 지금까지 받아왔던 페이지들 배열에서 삭제 (모두 한번씩 다 받아온 경우 그냥 랜덤으로 뿌림)
-      if (allPageParams.length < pagesCount) {
-        totalPageArray = totalPageArray.filter(
-          (n) => !allPageParams.includes(n)
-        );
-      }
-
-      // 랜덤 인덱스 선택
-      const randomIndex = Math.floor(Math.random() * totalPageArray.length);
-
-      return totalPageArray[randomIndex];
-    }
-    return 1;
-  };
-
-  const goToLink = ({ url }: { url: string }) => {
-    window.open(url);
-  };
-
-  // 데이터 추가 요청
-  const pushMore = async () => {
-    if (!isFetchingNextPage) {
-      await fetchNextPage();
-    }
-  };
-
-  const { data: contentsCountData } = useQuery({
-    queryKey: ["contentsCountData", searchParams],
-    queryFn: () => getContentsCount(searchParams),
-    staleTime: 5 * 1000 * 60,
-    gcTime: 30 * 1000 * 60,
-  });
 
   const {
     data: shuffledContentsData,
@@ -75,14 +36,25 @@ export default function ContentSlider() {
   } = useInfiniteQuery({
     queryKey: ["shuffledContents", searchParams],
     queryFn: ({ pageParam }) => getShuffledContents(pageParam, searchParams),
-    initialPageParam: getRandomNumber([]),
+    initialPageParam: getRandomNumber([], contentsCountData),
+    initialData: initialData,
     getNextPageParam: (_, __, ___, allPageParams) => {
-      return getRandomNumber(allPageParams);
+      return getRandomNumber(allPageParams, contentsCountData);
     },
     enabled: contentsCountData !== undefined,
     staleTime: 5 * 1000 * 60,
     gcTime: 30 * 1000 * 60,
   });
+
+  // 데이터 추가 요청
+  const pushMore = async () => {
+    if (!isFetchingNextPage) {
+      await fetchNextPage();
+    }
+  };
+  const goToLink = ({ url }: { url: string }) => {
+    window.open(url);
+  };
 
   // 스크롤 포지션 받아오기
   useEffect(() => {
@@ -100,15 +72,12 @@ export default function ContentSlider() {
           setScrollPosition(scrollPosition);
         }
       }
-      setContentsData(
-        shuffledContentsData.pages.map((page) => page.content).flat()
-      );
     }
   }, [shuffledContentsData, isStale, isFetchedAfterMount]);
 
   return (
     <div className="swiper-container">
-      {contentsData && (
+      {shuffledContentsData.pages.map((page) => page.content).flat() && (
         <Swiper
           modules={[Mousewheel]}
           mousewheel={{
@@ -123,112 +92,127 @@ export default function ContentSlider() {
               "scrollPosition",
               prop.activeIndex.toString()
             );
-            if (prop.activeIndex === contentsData.length - pagesBeforeFetch) {
+            if (
+              prop.activeIndex ===
+              shuffledContentsData.pages.map((page) => page.content).flat()
+                .length -
+                pagesBeforeFetch
+            ) {
               pushMore();
             }
           }}
         >
-          {contentsData.map((content) => {
-            const summaryArray = content.summary
-              .split("\n")
-              .map((item) => item.trim())
-              .filter((item) => item);
+          {shuffledContentsData.pages
+            .map((page) => page.content)
+            .flat()
+            .map((content) => {
+              const summaryArray = content.summary
+                .split("\n")
+                .map((item: any) => item.trim())
+                .filter((item: any) => item);
 
-            return (
-              <SwiperSlide key={content.id} className={styles.swiperSlide}>
-                <div className={styles.slideContainer}>
-                  <h1 className={styles.dateText}>{content.publishedDate}</h1>
-                  <div className={styles.titleBox}>
-                    <Image
-                      src={
-                        content.providerIconUrl
-                          ? content.providerIconUrl
-                          : no_image.src
-                      }
-                      alt={"provider icon"}
-                      width={30}
-                      height={30}
-                      style={{ borderRadius: 7 }}
-                      className={styles.providerIcon}
-                      onClick={() =>
-                        goToLink({
-                          url: content.providerUrl,
-                        })
-                      }
-                    ></Image>
-                    <div className={styles.titleWrap}>
-                      <h2
-                        className={styles.providerTitle}
-                        onClick={() => goToLink({ url: content.providerUrl })}
-                      >
-                        {content.providerTitle}
-                      </h2>
-                      <h2
-                        className={styles.title}
+              return (
+                <SwiperSlide key={content.id} className={styles.swiperSlide}>
+                  <div className={styles.slideContainer}>
+                    <h1 className={styles.dateText}>{content.publishedDate}</h1>
+                    <div className={styles.titleBox}>
+                      <Image
+                        src={
+                          content.providerIconUrl
+                            ? content.providerIconUrl
+                            : no_image.src
+                        }
+                        alt={"provider icon"}
+                        width={30}
+                        height={30}
+                        style={{ borderRadius: 7 }}
+                        className={styles.providerIcon}
+                        onClick={() =>
+                          goToLink({
+                            url: content.providerUrl,
+                          })
+                        }
+                      ></Image>
+                      <div className={styles.titleWrap}>
+                        <h2
+                          className={styles.providerTitle}
+                          onClick={() => goToLink({ url: content.providerUrl })}
+                        >
+                          {content.providerTitle}
+                        </h2>
+                        <h2
+                          className={styles.title}
+                          onClick={() =>
+                            goToLink({
+                              url: `${BASE_URL}/contents/${content.id}/link`,
+                            })
+                          }
+                        >
+                          {content.title}
+                        </h2>
+                      </div>
+                    </div>
+                    <div className={styles.summaryBox}>
+                      <div
+                        className={styles.summaryBtn}
                         onClick={() =>
                           goToLink({
                             url: `${BASE_URL}/contents/${content.id}/link`,
                           })
                         }
                       >
-                        {content.title}
-                      </h2>
-                    </div>
-                  </div>
-                  <div className={styles.summaryBox}>
-                    <div
-                      className={styles.summaryBtn}
-                      onClick={() =>
-                        goToLink({
-                          url: `${BASE_URL}/contents/${content.id}/link`,
-                        })
-                      }
-                    >
-                      <div className={styles.imgBox}>
-                        <Image
-                          src={
-                            content.imageUrl ? content.imageUrl : no_image.src
-                          }
-                          alt="content image"
-                          fill
-                          sizes="(max-height:250px)"
-                          priority={true}
-                          style={{
-                            objectFit: "cover",
-                            borderTopLeftRadius: 10,
-                            borderTopRightRadius: 10,
-                            width: "100%",
-                          }}
-                        ></Image>
+                        <div className={styles.imgBox}>
+                          <Image
+                            src={
+                              content.imageUrl ? content.imageUrl : no_image.src
+                            }
+                            alt="content image"
+                            fill
+                            sizes="(max-height:250px)"
+                            priority={true}
+                            style={{
+                              objectFit: "cover",
+                              borderTopLeftRadius: 10,
+                              borderTopRightRadius: 10,
+                              width: "100%",
+                            }}
+                          ></Image>
+                        </div>
+                        <div className={styles.summary}>
+                          {summaryArray.map(
+                            (summary: string, index: number) => {
+                              return (
+                                <div
+                                  key={index}
+                                  className={styles.summaryTextBox}
+                                >
+                                  <IndexIndicator index={index} />
+                                  <h2 className={styles.summaryText}>
+                                    {summary.replace(/^\d+\.\s*/, "")}
+                                  </h2>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
                       </div>
-                      <div className={styles.summary}>
-                        {summaryArray.map((summary, index) => {
-                          return (
-                            <div key={index} className={styles.summaryTextBox}>
-                              <IndexIndicator index={index} />
-                              <h2 className={styles.summaryText}>
-                                {summary.replace(/^\d+\.\s*/, "")}
-                              </h2>
-                            </div>
-                          );
-                        })}
-                      </div>
-                    </div>
 
-                    <div className={styles.categoryBox}>
-                      {content.categories.map((category, index) => {
-                        return (
-                          <h2 key={index} className={styles.categoryText}>
-                            #{Categories[category]}
-                          </h2>
-                        );
-                      })}
+                      <div className={styles.categoryBox}>
+                        {content.categories.map(
+                          (category: string, index: number) => {
+                            return (
+                              <h2 key={index} className={styles.categoryText}>
+                                #{Categories[category]}
+                              </h2>
+                            );
+                          }
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </SwiperSlide>
-            );
-          })}
+                </SwiperSlide>
+              );
+            })}
         </Swiper>
       )}
     </div>
--- src/app/(home)/_components/Home.tsx
+++ src/app/(home)/_components/Home.tsx
@@ -4,7 +4,17 @@ import ContentSlider from "./ContentSlider";
 import { Suspense } from "react";
 import { contentFallBack } from "./ContentSliderFallback";
 import { categoryFallback } from "@/app/_components/CategoryFallback";
-export default function Home() {
+import { getContentsCount, getShuffledContents } from "@/app/_utils/api";
+import getRandomNumber from "@/app/_utils/getRandomNumber";
+export default async function Home() {
+  const contentsCountData = await getContentsCount("");
+  const randomIndex = getRandomNumber([], contentsCountData);
+  const initialContents = await getShuffledContents(randomIndex, "");
+  const initialData = {
+    pages: [initialContents],
+    pageParams: [randomIndex],
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.contentBox}>
@@ -13,7 +23,10 @@ export default function Home() {
         </Suspense>
         <div className={styles.sliderBox}>
           <Suspense fallback={contentFallBack}>
-            <ContentSlider />
+            <ContentSlider
+              initialData={initialData}
+              contentsCountData={contentsCountData}
+            />
           </Suspense>
         </div>
       </div>
